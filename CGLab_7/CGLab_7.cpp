550a103721009d10e1b6e0a4bb14489b82e35554
Gaifullin Damir <101250473+GaifullinDamir@users.noreply.github.com>
2022-04-07 13:52:02 +0300

    Final

--- CGLab_7/CGLab_7.cpp
+++ CGLab_7/CGLab_7.cpp
@@ -24,7 +24,7 @@ void main(int argc, char** argv)
 
 void init()
 {
-	glClearColor(0.07, 0.2, 0.34, 1.0); // задаём значение очистки цветом буфера цвета(цвет фона)
+	glClearColor(1.0, 1.0, 1.0, 1.0); // задаём значение очистки цветом буфера цвета(цвет фона)
 	glMatrixMode(GL_PROJECTION); // задаём текущей матрицу проекции
 	glLoadIdentity(); // устанавливаем единичную матрицу
 	gluOrtho2D(-100.0, 100.0, -100.0, 100.0); // ортогональная проекция - плоскость Oxy
@@ -32,11 +32,12 @@ void init()
 
 void draw()
 {
-	GLfloat A = 100, B = 50, X1 = 0, Y1 = 0;
+	GLfloat A = 95, B = 55 , X1 = 0, Y1 = 0;
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glViewport(0, 0, 600, 600);
 	glColor3f(0.0, 0.0, 0.0);
 
+	glLineWidth(1);
 	glBegin(GL_LINES); //рисуем оси
 	glVertex2f(-100.0, 0.0); glVertex2f(100.0, 0.0);
 	glVertex2f(0.0, 100.0); glVertex2f(0.0, -100.0);
@@ -46,39 +47,42 @@ void draw()
 	////если F(x, y) < 0, то y : = y + 1, делается шаг + 1 по y;
 	////если F(x, y) > 0, то x : = x + 1, делается шаг + 1 по x;
 	////если F(x, y) = 0, то x : = x + 1 при A>B;
-	glColor3f(0.0, 0.0, 1.0);
-	glLineWidth(3);
+	glColor3f(0.0, 0.565, 0.33);
+	glLineWidth(2);
 	glBegin(GL_LINE_STRIP);
 	while (X1 < A) {
 		glVertex2f(X1, Y1);
 		if ((A * Y1 - B * X1) < 0) {
-			Y1 = Y1 + 0.01;
+			Y1 = Y1 + 1;
 			glVertex2f(X1, Y1);
 		}
 		else if ((A * Y1 - B * X1) > 0) {
-			X1 = X1 + 0.01;
+			X1 = X1 + 1;
 			glVertex2f(X1, Y1);
 		}
 		else {
-			X1 = X1 + 0.01;
+			X1 = X1 + 1;
 			glVertex2f(X1, Y1);
 		}
 	}
 	glEnd();
 
 	//методом цифровых дифференциальных анализаторов .	
-	GLfloat dX, dY, X2, Y2, C = -100, D = 100;
+	GLfloat dX, dY, X2, Y2, C = -70, D = 55;
 	X2 = 0, Y2 = 0,
-		glColor3f(0.5, 0.0, 0.5);
+	glColor3f(1.0, 0.745, 0.137);
+	glLineWidth(3);
 	glBegin(GL_LINE_STRIP);
 	dX = C - X2;
 	dY = D - Y2;
 	glVertex2f(X2, Y2);
+
 	while (Y2 < D) {
 		Y2 = Y2 + 1;
 		X2 = X2 + dY / dX;
 		glVertex2f(X2, Y2);
 	}
+	
 	glEnd();
 	glFlush();
 
